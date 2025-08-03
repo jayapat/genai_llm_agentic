39f2be867c2f4bc15ada1360e7c9d903fe96abbb
jayapat <pjay271024@gmail.com>
2025-08-03 13:30:47 -0700

    Add .env and .gitignore files, implement X post generation in main.py, and update dependencies in pyproject.toml

--- main.py
+++ main.py
@@ -1,6 +1,48 @@
-def main():
-    print("Hello from genai-llm-agentic!")
+import os
+import requests
+from dotenv import load_dotenv
 
+load_dotenv()
+
+def generate_x_post(topic: str) -> str:
+    prompt = f"""
+        You are an expert social media manager, and you excel at crafting viral and highly engaging posts for X (formerly Twitter).
+
+        Your task is to generate a post that is concise, impactful, and tailored to the topic provided by the user.
+        Avoid using hashtags and lots of emojis (a few emojis are okay, but not too many).
+
+        Keep the post short and focused, structure it in a clean, readable way, using line breaks and empty lines to enhance readability.
+
+        Here's the topic provided by the user for which you need to generate a post:
+        <topic>
+        {topic}
+        </topic>
+    """
+    payload = {
+        "model": "gpt-4o",
+        "messages": [
+            {"role": "user", "content": prompt}
+        ]
+    }
+    response = requests.post(
+        "https://api.openai.com/v1/chat/completions",
+        json=payload,
+        headers={
+            "Content-Type": "application/json",
+            "Authorization": "Bearer ",
+        },
+    )
+
+    if response.status_code == 200:
+        return response.json()["choices"][0]["message"]["content"]
+    else:
+        return f"Error: {response.status_code} - {response.text}"
+
+def main():
+    usr_input = input("What should the post be about? ")
+    x_post = generate_x_post(usr_input)
+    print("Generated X post")
+    print(x_post)
 
 if __name__ == "__main__":
     main()
